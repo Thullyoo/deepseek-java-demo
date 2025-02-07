32be2198591c120c064456df5007100aa73bf1e3
Thullyoo <thullyofilho@gmail.com>
2025-02-07 11:41:01 -0300

    Initial commit

---
+++ src/main/java/br/thullyoo/deepseek_java_demo/DeepseekJavaDemoApplication.java
@@ -0,0 +1,13 @@
+package br.thullyoo.deepseek_java_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeepseekJavaDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeepseekJavaDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/thullyoo/deepseek_java_demo/DeepseekJavaDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.thullyoo.deepseek_java_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeepseekJavaDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
